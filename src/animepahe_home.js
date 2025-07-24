ac64b0d5d771c01b5d37dea74a0e1203480e42ef
Nounours0261 <nounours.boi@gmail.com>
2025-07-25 01:10:09 +0200

    fix(download): fallback title

--- src/animepahe_home.js
+++ src/animepahe_home.js
@@ -1,8 +1,12 @@
 async function homeStartDL(res) {
+    let titleElement = document.querySelector("h2.japanese");
+    if (titleElement === null) {
+        titleElement = document.querySelector("h1.user-select-none");
+    }
     chrome.runtime.sendMessage({
         action: "downloadImage",
         url: document.querySelector(".poster-wrapper .anime-poster a").href,
-        filename: document.querySelector("h2.japanese").textContent.replace(/[^a-zA-Z0-9-.,()_]+/g, "_")
+        filename: titleElement.textContent.replace(/[^a-zA-Z0-9-.,()_]+/g, "_")
     });
 
     let isAsc = document.querySelector(".btn.btn-dark.btn-sm.active").innerText.includes("asc");
